5a64dde1196ed83cec6e9486c2da9c239819635f
zsoltadam92 <zsoltadam92@gmail.com>
2024-01-13 20:35:02 +0100

    search section

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,17 @@
 import markdown2
 from django import forms
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from . import util
 
 class QueryForm(forms.Form):
-    query = forms.CharField(label="")
+    query = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": QueryForm()
     })
 
 def getEntry(request,title):
@@ -29,3 +30,25 @@ def getEntry(request,title):
             "form": form,
             "message": message
         })
+
+def search(request):
+    entries = util.list_entries()
+
+    if request.method == "POST":
+        form = QueryForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            if util.get_entry(query):
+                return redirect("entry", title=query)
+            results = [entry for entry in entries if query.lower() in entry.lower()]
+            return render(request, "encyclopedia/search.html", {
+                "query": query,
+                "results": results,
+                "form": form
+            })
+        else: 
+            print(f"Form error: {form.errors}")
+    
+    return render(request, "encyclopedia/search.html", {
+        "form": QueryForm()
+    })
